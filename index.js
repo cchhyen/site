e553effae8ecfd142edd01966cca55e43ec8c104
cchhyen <gcvivady@gmail.com>
2025-09-08 01:48:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -10,22 +10,18 @@ document.getElementById("spinButton").addEventListener("click", function() {
     function numberchange() {
         if (i <= 10) {
             a = Math.floor(Math.random() * 100*number) + 2;
-            if (a == number*100 + 1) {
-                score = 999;
-            } else {
-                score = Math.floor(a / 100) + 1;
-            }
+            if (a == number*100 + 1) {score = 999;} 
+            else {score = Math.floor(a / 100) + 1;}
             document.getElementById("result").innerText = "결과: " + score;
             i = i + 1;
-            setTimeout(numberchange, 50);
-        } else {
-            document.getElementById("result").innerText = "결과: " + score;
-        }
+            setTimeout(numberchange, 50)} 
+        else{document.getElementById("result").innerText = "결과: " + score;}
     }
 
     numberchange();
 
 });
+
 
 
 
